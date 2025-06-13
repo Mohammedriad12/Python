5bb2c9d46816892112bbb03e71bec8a8168d91a4
M.alhaj_linux <3theflash666@gmail.com>
2025-06-13 18:10:47 +0300

    add the palindromeproblem

--- demo.py
+++ demo.py
@@ -41,7 +41,12 @@
 # #4. Palindrome Checker
 # # Write a function is_palindrome(word) that checks if a string is a palindrome (same forward and backward).
 
-def palindrome(args):
-    rev = args[::-1]
-    
-palindrome("ali")
+# def palindrome(args):
+#     rev = args[::-1]
+#     if args == rev:
+#         return True
+#     else:
+#         return False
+
+# print(palindrome("madam"))  # True
+# print(palindrome("hello"))  # False
